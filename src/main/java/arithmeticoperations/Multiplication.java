a720bdba88eddefbb6f6fd7697be31b734b0550b
Nicola Vitale <nico.vits@gmail.com>
2022-07-11 09:55:02 +0200

    created class Sum, Subtraction, Multiplication and Division

---
+++ src/main/java/arithmeticoperations/Multiplication.java
@@ -0,0 +1,13 @@
+package arithmeticoperations;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Multiplication {
+
+  private final String operator1;
+  private final String operator2;
+
+}
---
+++ src/main/java/arithmeticoperations/Subtraction.java
@@ -0,0 +1,13 @@
+package arithmeticoperations;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Subtraction {
+
+  private final String operator1;
+  private final String operator2;
+
+}
