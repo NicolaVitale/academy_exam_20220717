bcc922847f54f2dbc19eb92a3775d8f3be4bedae
Nicola Vitale <nico.vits@gmail.com>
2022-07-11 12:13:48 +0200

    implementation class MultiplcationDTO and Multiplication

--- src/main/java/arithmeticoperations/Multiplication.java
+++ src/main/java/arithmeticoperations/Multiplication.java
@@ -2,12 +2,18 @@ package arithmeticoperations;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import utils.ConvertOperator;
+import utils.CurrencyAmount;
 
 @Getter
 @AllArgsConstructor
 public class Multiplication {
 
-  private final String operator1;
-  private final String operator2;
+  private final CurrencyAmount operator1;
+  private final int operator2;
 
+  public CurrencyAmount multiplication() {
+    int penniesFirstOperand = ConvertOperator.currencyAmountToPennies(operator1);
+    return ConvertOperator.penniesToCurrencyAmount(penniesFirstOperand * operator2);
+  }
 }
---
+++ src/main/java/arithmeticoperations/MultiplicationDTO.java
@@ -0,0 +1,26 @@
+package arithmeticoperations;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import utils.ConvertOperator;
+import utils.CurrencyAmount;
+
+public class MultiplicationDTO extends Amount {
+
+  private final Pattern pattern =
+      Pattern.compile("^([-]?[0-9]*p)? ?([-]?[0-9]*s)?"
+          + " ?([-]?[0-9]*d)? ? \\* ([-]?[0-9]*)$");
+
+  public Multiplication multiplication(final String inputString) {
+    final Matcher matcher = pattern.matcher(inputString);
+
+    if (matcher.find()) {
+      CurrencyAmount firstOperand = extractCurrencyAmount(matcher.group(1),
+          matcher.group(2), matcher.group(3));
+
+      return new Multiplication(firstOperand, ConvertOperator.extractInt(matcher.group(4)));
+    }
+    return null;
+  }
+
+}
