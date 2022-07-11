d1be575cb18c3b945a4e51d87666e181ffb83073
Nicola Vitale <nico.vits@gmail.com>
2022-07-11 11:24:28 +0200

    Implemention class SumDTO and Sum

--- src/main/java/arithmeticoperations/Sum.java
+++ src/main/java/arithmeticoperations/Sum.java
@@ -2,12 +2,21 @@ package arithmeticoperations;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import utils.ConvertOperator;
+import utils.CurrencyAmount;
 
 @Getter
 @AllArgsConstructor
 public class Sum {
 
-  private final String operator1;
-  private final String operator2;
+  private final CurrencyAmount operator1;
+  private final CurrencyAmount operator2;
 
+  public CurrencyAmount sumOperation() {
+
+    int penniesOperator1 = ConvertOperator.currencyAmountToPennies(operator1);
+    int penniesOperator2 = ConvertOperator.currencyAmountToPennies(operator2);
+    return ConvertOperator.penniesToCurrencyAmount(penniesOperator1 + penniesOperator2);
+
+  }
 }
---
+++ src/main/java/arithmeticoperations/SumDTO.java
@@ -0,0 +1,26 @@
+package arithmeticoperations;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import utils.CurrencyAmount;
+
+public class SumDTO extends Amount {
+    private Pattern pattern =
+        Pattern.compile("^([-]?[0-9]*p)? ?([-]?[0-9]*s)? ?([-]?[0-9]*d)? ? "
+            + "\\+ ([-]?[0-9]*p)? ?([-]?[0-9]*s)? ?([-]?[0-9]*d)?$");
+
+    public Sum SumDTO (final String inputString) {
+      Matcher matcher = pattern.matcher(inputString);
+
+      if (matcher.find()) {
+
+        CurrencyAmount firstOperand = extractCurrencyAmount(matcher.group(1),
+            matcher.group(2), matcher.group(3));
+        CurrencyAmount secondOperand = extractCurrencyAmount(matcher.group(4),
+            matcher.group(5), matcher.group(6));
+
+        return new Sum(firstOperand, secondOperand);
+      }
+      return null;
+    }
+}
